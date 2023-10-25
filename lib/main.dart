220a7870404c0aaec97c50fd072b06959c76afb6
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-25 23:08:05 +0500

    image name email icon added

--- lib/main.dart
+++ lib/main.dart
@@ -11,39 +11,87 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey[600],
-        title:  const Center(
-          child:  Text('Bufon First App')
+        backgroundColor: Colors.blueGrey[400],
+        title: const Text('Bufon Ninga Card App'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: const Padding(
+
+        padding: EdgeInsets.all(50),
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child:CircleAvatar(
+                backgroundImage: AssetImage('assets/bufon.jpg'),
+                radius: 40,
+              )
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey,
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Bufon',
+              style: TextStyle(
+                color: Colors.cyan,
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Current Ninja Level',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.cyan,
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Icon(Icons.email),
+                SizedBox(width: 10),
+                Text(
+                  "bufon@bufon.com",
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    letterSpacing: 2
+                  ),
+                  ),
+                
+              ],
+            ),
+            
+          ],
+
+          
         ),
       ),
-      body:Row(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.red,
-              padding: const EdgeInsets.all(20.0),
-              child: const Text('1'),
-              
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.blue,
-              padding: const EdgeInsets.all(20.0),
-              child: const Text('2'),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.green,
-              padding: const EdgeInsets.all(20.0),
-              child: const Text('3'),
-            ),
-          ),
-        ],
-      )
     );
   }
 }
