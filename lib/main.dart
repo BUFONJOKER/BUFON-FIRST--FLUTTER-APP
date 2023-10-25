eb04b6a412d6932fe646428a4da61abd3e76e8b6
ABDUL REHMAN JAVAID <134438770+BUFONJOKER@users.noreply.github.com>
2023-10-25 23:27:12 +0500

    stateless widget change to stte ful and ninja level change when clicking floating action button with plus icon

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,16 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var bufonNinjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,32 +27,38 @@ class Home extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
       ),
-      body: const Padding(
-
-        padding: EdgeInsets.all(50),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueGrey[400],
+        child: const Icon(Icons.add),
+        onPressed: ()=> {
+          setState(() {
+            bufonNinjaLevel +=1;
+          })
+        }),
         
+      body: Padding(
+        padding: const EdgeInsets.all(50),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Center(
-              child:CircleAvatar(
-                backgroundImage: AssetImage('assets/bufon.jpg'),
-                radius: 40,
-              )
-            ),
-            Divider(
+            const Center(
+                child: CircleAvatar(
+              backgroundImage: AssetImage('assets/bufon.jpg'),
+              radius: 40,
+            )),
+            const Divider(
               height: 60,
               color: Colors.grey,
             ),
-            Text(
+            const Text(
               'Name',
               style: TextStyle(
                 color: Colors.blueGrey,
                 letterSpacing: 2,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               'Bufon',
               style: TextStyle(
                 color: Colors.cyan,
@@ -53,45 +67,39 @@ class Home extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               'Current Ninja Level',
               style: TextStyle(
                 color: Colors.blueGrey,
                 letterSpacing: 2,
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Text(
-              '8',
-              style: TextStyle(
+              '$bufonNinjaLevel',
+              style: const TextStyle(
                 color: Colors.cyan,
                 letterSpacing: 2,
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            Row(
+            const SizedBox(height: 20),
+            const Row(
               children: [
                 Icon(Icons.email),
                 SizedBox(width: 10),
                 Text(
                   "bufon@bufon.com",
-                  style: TextStyle(
-                    color: Colors.cyan,
-                    letterSpacing: 2
-                  ),
-                  ),
-                
+                  style: TextStyle(color: Colors.cyan, letterSpacing: 2),
+                ),
               ],
             ),
-            
           ],
-
-          
         ),
       ),
     );
   }
 }
+
